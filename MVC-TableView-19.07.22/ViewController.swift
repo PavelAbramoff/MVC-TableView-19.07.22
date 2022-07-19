557b9090bd4656ce6f22da95e28a1bffd7befbbd
Pavel <pavel.818@yandex.ru>
2022-07-19 16:35:49 +0400

    Initial Commit

---
+++ MVC-TableView-19.07.22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVC-TableView-19.07.22
+//
+//  Created by Pavel Абрамов on 19.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
