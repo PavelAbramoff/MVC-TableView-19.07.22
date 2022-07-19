16584b462657a3aa80460cbed994f3c75950e5ef
Pavel <pavel.818@yandex.ru>
2022-07-19 17:36:37 +0400

    MVC

---
+++ MVC-TableView-19.07.22/TableView.swift
@@ -0,0 +1,72 @@
+//
+//  TableView.swift
+//  MVC-TableView-19.07.22
+//
+//  Created by Pavel Абрамов on 19.07.2022.
+//
+
+import UIKit
+
+class TableView: UIView {
+
+    private var controller: ViewController?
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+
+    init(controller: ViewController) {
+        super.init(frame: .zero)
+        self.controller = controller
+        commonInit()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+        setupHierarchy()
+        setupLayout()
+        
+    }
+    
+    private func commonInit() {
+        tableView.register(UITableViewCell.self,forCellReuseIdentifier: "cell")
+        tableView.delegate = self
+        tableView.dataSource = self
+        setupHierarchy()
+    }
+    
+    private func setupHierarchy() {
+        addSubview(tableView)
+    }
+    
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+    
+}
+
+extension TableView: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "Cell \(indexPath.row + 1)"
+        return cell
+    }
+    
+    
+}
+
+extension TableView: UITableViewDelegate {
+    
+}
